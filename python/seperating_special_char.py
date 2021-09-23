f0891f0d8bbbe16ec215e3c51b266ff9d363ff1f
rishavmitra <72710483+rishavmitra@users.noreply.github.com>
2021-09-23 16:05:51 +0530

    Update seperating_special_char.py

--- python/seperating_special_char.py
+++ python/seperating_special_char.py
@@ -1,1 +1,12 @@
+str = input()
+
+str=''.join(e for e in str if e.isalnum())  #isalpha() returns true if the value is a number or an alphabet
+str = str.lower()
+
+str1 = str[-1::-1]
+if(str==str1):
+  print('Yes')
+else:
+  print('No')
+
 
